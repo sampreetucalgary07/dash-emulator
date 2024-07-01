2a48ef3e4022fb73128fc4a4aaf5ebc2712b1948
sampreetucalgary07 <sampreet.vaidya@ucalgary.ca>
2024-07-01 16:57:54 -0600

    tried adding states in event logger

--- dash_emulator/event_logger.py
+++ dash_emulator/event_logger.py
@@ -5,6 +5,10 @@ from dash_emulator.scheduler import SchedulerEventListener
 
 
 class EventLogger(SchedulerEventListener, PlayerEventListener):
+    def __init__(self) -> None:
+        super().__init__()
+        self.states = []
+
     log = logging.getLogger("EventLogger")
 
     async def on_buffer_level_change(self, buffer_level):
@@ -13,6 +17,7 @@ class EventLogger(SchedulerEventListener, PlayerEventListener):
     async def on_state_change(
         self, position: float, old_state: State, new_state: State
     ):
+        self.states.append([position, old_state, new_state])
         self.log.info(
             "Switch state. pos: %.3f, from %s to %s" % (position, old_state, new_state)
         )
@@ -24,3 +29,6 @@ class EventLogger(SchedulerEventListener, PlayerEventListener):
 
     async def on_segment_download_complete(self, index):
         self.log.info("Download complete. Index: %d" % index)
+
+    def get_states(self):
+        return self.states
